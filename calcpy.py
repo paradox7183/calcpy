810510d932a9b9b59f97caf6c165344df0d3f192
GrayFox <41882875+paradox7183@users.noreply.github.com>
2022-12-24 20:32:28 -0600

    Update calcpy.py
    Better Sintaxis and adding the function for the second and third derivate from a function

--- calcpy.py
+++ calcpy.py
@@ -11,19 +11,19 @@ from sympy import *
 from sympy import symbols, Eq, solve, sqrt
 #Definicion de simbolos algebraicos 
 x, y, z, a, b, c, d, t = sym.symbols('x y z a b c d t')
-#Derivada de una funcion 
+#Input para poder expresar la funcion 
 dx = parse_expr(input(Fore.GREEN + Style.BRIGHT +"Introduce la funcion>" + Style.RESET_ALL))
-print(Fore.YELLOW + Style.BRIGHT + "Calculando la derivada..." + Style.RESET_ALL )
-print (".")
-print (".")
+#Empezando a derivar e integrar 
 yprime = dx.diff(x)
-dxderivate = Derivative(dx,x,a,b).doit()
+sndv = dx.diff(x,x)
+thdv = dx.diff(x,x,x)
 yintegrate = integrate (dx) 
-#Mostrando el resultado de la derivada 
+#Mostrando el resultado de la primera, segunda, tercera derivada y de la integral
 print(Fore.RED + Style.BRIGHT + "Derivada: " +Style.RESET_ALL)
 print(yprime)
-print (Fore.GREEN + Style.BRIGHT + "Integración: ")
+print (Fore.YELLOW + Style.BRIGHT +"Segunda Derivada de la funcion:")
+print (sndv)
+print (Fore.CYAN + Style.BRIGHT +"Tercera derivada de la funcion:")
+print (thdv)
+print (Fore.GREEN + Style.BRIGHT + "Integral: ")
 print (yintegrate)
-print (Fore.BLUE + Style.BRIGHT+ "Resultado en base a todas las variables:")
-simplify(dxderivate)
-print (dxderivate) 
